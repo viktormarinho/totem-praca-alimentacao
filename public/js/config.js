5b3cfafbf18d0b8d0dba5bbacb719512fda5a8f1
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-07-04 13:29:11 -0300

    Delete config.js

--- public/js/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "API KEY REMOVIDA POR SEGURANÇA DO REPOSITÓRIO PÚBLICO... trabalho já finalizado",
-    authDomain: "hare-somativa.firebaseapp.com",
-    projectId: "hare-somativa",
-    storageBucket: "hare-somativa.appspot.com",
-    messagingSenderId: "409735897177",
-    appId: "1:409735897177:web:4060861dabad5e9e9d5148",
-    measurementId: "G-D9HYK39ELE"
-};
